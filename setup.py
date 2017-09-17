793966a3c3299b961567201b5df236ce0748f865
Atsushi Odagiri <aodagx@gmail.com>
2017-09-17 13:32:01 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="wsgi-objgraph",
+    setup_require=["setuptools_scm"],
+    use_scm_version=True,
+    author="Atsushi Odagiri",
+    author_email="aodagx@gmail.com",
+    packages=find_packages(exclude=["example", "tests"]),
+    install_requires=[
+        "objgraph",
+    ],
+    extras_require={
+        "testing": [
+            "pytest",
+            "webtest",
+        ],
+    },
+)
